e9afe790674b2ad53b4c5b0891133e39bcb901c6
Kyle Barnett <kbarnett23@gmail.com>
2019-04-03 21:48:16 -0600

    finished student

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,22 +1,22 @@
 // CODE here for your Lambda Classes
 class Person {
-  constructor(props){
+  constructor(props) {
     this.name = props.name;
     this.age = props.age;
     this.location = props.location;
     this.gender = props.gender;
   }
-  speak(){
+  speak() {
     console.log(`Hello my name is ${this.name}, and I am from ${this.location}`)
   }
 }
-const myself = new Person ({
+const myself = new Person({
   name: 'Kyle',
   age: 23,
   location: 'Salt Lake City',
   gender: 'Male'
 })
-const girlfriend = new Person ({
+const girlfriend = new Person({
   name: 'Priscilla',
   age: 24,
   location: 'Salt Lake City',
@@ -27,16 +27,16 @@ girlfriend.speak()
 
 
 class Instructor extends Person {
-  constructor(props){
+  constructor(props) {
     super(props)
     this.specialty = props.specialty;
     this.favLanguage = props.favLanguage;
     this.catchPhrase = props.catchPhrase;
   }
-  demo(subject){
+  demo(subject) {
     console.log(`Today we are learning about ${subject}`)
   }
-  grade(student, subject){
+  grade(student, subject) {
     console.log(`${student.name} receives a perfect score on ${subject}`)
   }
 }
@@ -51,4 +51,48 @@ const teacher1 = new Instructor({
   catchPhrase: `Don't forget the homies`
 })
 teacher1.demo('Python')
-teacher1.grade({name: 'Kyle'}, 'Javascript')
+teacher1.grade({ name: 'Kyle' }, 'Javascript')
+
+const teacher2 = new Instructor({
+  name: 'Marry',
+  age: 36,
+  location: 'Salt Lake City',
+  gender: 'Female',
+  specialty: 'History',
+  favLanguage: 'HTML',
+  catchPhrase: `Don't forget the homies`
+})
+
+class Student extends Person {
+  constructor(props) {
+    super(props)
+    this.previousBackground = props.previousBackground;
+    this.className = props.className;
+    this.favSubjects = props.favSubjects;
+  }
+  listSubjects() {
+    this.favSubjects.map(favSubjects => {
+      console.log(favSubjects)
+    })
+  }
+  PRAssignment(subject) {
+    console.log(`${this.name} has submitted a PR for ${subject}`)
+  }
+  sprintChallenge(subject) {
+    console.log(`${this.name} has begun sprint challenge on ${subject}`)
+  }
+}
+
+const student1 = new Student({
+  name: 'Jerry',
+  age: 21,
+  location: 'NYC',
+  gender: 'Male',
+  previousBackground: 'Athlete',
+  className: 'WEBPT5',
+  favSubjects: ['Javascript, HTML, CSS']
+})
+
+student1.listSubjects()
+student1.PRAssignment('Javascript IV')
+student1.sprintChallenge('Javascript IV')
