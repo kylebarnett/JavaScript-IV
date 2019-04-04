70b7b450f3104d91317da733fe2964b07984249a
Kyle Barnett <kbarnett23@gmail.com>
2019-04-03 19:50:50 -0600

    finished Person

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,2 +1,15 @@
 // CODE here for your Lambda Classes
-
+class Person {
+  constructor(name, age, location, gender){
+    this.name = name;
+    this.age = age;
+    this.location = location;
+    this.gender = gender;
+  }
+  speak(){
+    console.log(`Hello my name is ${this.name}, and I am from ${this.location}`)
+  }
+}
+const hello = new Person('Kyle', 23, 'SLC', 'Male')
+// console.log(hello)
+console.log(hello.speak())
