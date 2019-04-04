9e356a0dad3f385ea1ba1456704a6b6201c2ec5b
Kyle Barnett <kbarnett23@gmail.com>
2019-04-03 20:45:08 -0600

    finished instructor I think

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,15 +1,54 @@
 // CODE here for your Lambda Classes
 class Person {
-  constructor(name, age, location, gender){
-    this.name = name;
-    this.age = age;
-    this.location = location;
-    this.gender = gender;
+  constructor(props){
+    this.name = props.name;
+    this.age = props.age;
+    this.location = props.location;
+    this.gender = props.gender;
   }
   speak(){
     console.log(`Hello my name is ${this.name}, and I am from ${this.location}`)
   }
 }
-const hello = new Person('Kyle', 23, 'SLC', 'Male')
-// console.log(hello)
-console.log(hello.speak())
+const myself = new Person ({
+  name: 'Kyle',
+  age: 23,
+  location: 'Salt Lake City',
+  gender: 'Male'
+})
+const girlfriend = new Person ({
+  name: 'Priscilla',
+  age: 24,
+  location: 'Salt Lake City',
+  gender: 'Female'
+})
+myself.speak()
+girlfriend.speak()
+
+
+class Instructor extends Person {
+  constructor(props){
+    super(props)
+    this.specialty = props.specialty;
+    this.favLanguage = props.favLanguage;
+    this.catchPhrase = props.catchPhrase;
+  }
+  demo(subject){
+    console.log(`Today we are learning about ${subject}`)
+  }
+  grade(student, subject){
+    console.log(`${student.name} receives a perfect score on ${subject}`)
+  }
+}
+
+const teacher1 = new Instructor({
+  name: 'Hsu',
+  age: 45,
+  location: 'Salt Lake City',
+  gender: 'Male',
+  specialty: 'Mathamatics',
+  favLanguage: 'Javascript',
+  catchPhrase: `Don't forget the homies`
+})
+teacher1.demo('Python')
+teacher1.grade({name: 'Kyle'}, 'Javascript')
